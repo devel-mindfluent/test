cb30f72198723f25dc6622d52b428985aa7f9765
Devs@Mindfluent <78065496+devel-mindfluent@users.noreply.github.com>
2023-02-13 12:29:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,9 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups')
-st.map(data)
+# st.subheader('Map of all pickups')
+# st.map(data)
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
